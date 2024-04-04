1049b74253429354425d3146e349db33c2032f9d
Erik Antunes <erik.antunes@live.com>
2024-04-04 16:52:03 -0300

    adicionando MaterialApp

--- learn_application/lib/main.dart
+++ learn_application/lib/main.dart
@@ -10,10 +10,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context){
-    return const Center(
-      child: Text(
-      'Erikão',
-      textDirection: TextDirection.rtl
+    return const MaterialApp(
+      home: HomePage(),
+        );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: const Center(
+        child: Text('Erikão'),
       ),
     );
   }
