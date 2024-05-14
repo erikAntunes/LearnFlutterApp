4b7acc85751cdfc176ca1ab365e8eac5678352cd
Erik Antunes <erik.antunes@live.com>
2024-05-14 15:28:26 -0300

    separando Widgets em arquivos individuais

--- learn_application/lib/main.dart
+++ learn_application/lib/main.dart
@@ -1,29 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:learn_application/myapp.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      home: HomePage(),
-        );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  @override
-  Widget build(BuildContext context){
-    return const Material(
-      child: Center(
-        child: Text('Erikão'),
-      ),
-    );
-  }
+  runApp(const MyApp() as Widget);
 }
 
